8a4e9b485d582352ab928ddb742675d77b0cbca5
nttmai18it2 <44257679+nttmai18it2@users.noreply.github.com>
2021-05-13 21:52:50 +0700

    Add files via upload

---
+++ Bai1.py
@@ -0,0 +1,14 @@
+import unittest
+from selenium import webdriver
+
+class Test(unittest.TestCase):
+    def testName(self):
+        chrome_driver_path = "C:/Users/ADMIN/Documents/KiemThu/chromedriver.exe"
+        driver = webdriver.Chrome(chrome_driver_path)
+
+        driver.get("http://practice.automationtesting.in/")
+        title = driver.title
+        self.assertTrue(title == "Automation Practice Site")
+        
+if  __name__ == "__main__":
+    unittest.main()
---
+++ Bai3.py
@@ -0,0 +1,15 @@
+import unittest
+from selenium import webdriver
+
+class Test(unittest.TestCase):
+    def testName(self):
+        chrome_driver_path = "C:/Users/ADMIN/Documents/KiemThu/chromedriver.exe"
+        driver = webdriver.Chrome(chrome_driver_path)
+
+        driver.get("http://practice.automationtesting.in/")
+        title = driver.title
+        driver.set_window_size(500,400)
+        self.assertTrue(title == "Automation Practice Site")
+        
+if  __name__ == "__main__":
+    unittest.main()
